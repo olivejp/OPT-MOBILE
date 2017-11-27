71ade3cb7aadb129467a6c4ce957f3357b5de4b1
2761oli <jean-paul.olive@opt.nc>
2017-11-27 15:07:02 +1100

    Fix : Delete useless comments

--- app/src/androidTest/java/nc/opt/mobile/optmobile/service/TransformHtmlTest.java
+++ app/src/androidTest/java/nc/opt/mobile/optmobile/service/TransformHtmlTest.java
@@ -16,11 +16,6 @@ import nc.opt.mobile.optmobile.utils.Utilities;
 
 import static nc.opt.mobile.optmobile.utils.HtmlTransformer.RESULT_SUCCESS;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class TransformHtmlTest {
 
